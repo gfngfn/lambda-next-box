15049ff11908424a31458923c858e1138952f847
suwa <5546507742@mail.ecc.u-tokyo.ac.jp>
2016-08-07 21:15:26 +0900

    developed single beta-reduction function without alpha-renaming

--- evaluator.ml
+++ evaluator.ml
@@ -3,6 +3,23 @@ open Types
 type eval_state = Stable | Changed
 
 
+let delta_reduction (ast : abstract_tree) =
+  let (astmain, layer) = ast in
+    match astmain with
+    | Apply((Apply((PermContentOf("+"), _), (IntConst(ic1), _)), _), (IntConst(ic2), _))    -> (Changed, (IntConst(ic1 + ic2), layer))
+    | Apply((Apply((PermContentOf("-"), _), (IntConst(ic1), _)), _), (IntConst(ic2), _))    -> (Changed, (IntConst(ic1 - ic2), layer))
+    | Apply((Apply((PermContentOf("*"), _), (IntConst(ic1), _)), _), (IntConst(ic2), _))    -> (Changed, (IntConst(ic1 * ic2), layer))
+    | Apply((Apply((PermContentOf("/"), _), (IntConst(ic1), _)), _), (IntConst(ic2), _))    -> (Changed, (IntConst(ic1 / ic2), layer))
+    | Apply((Apply((PermContentOf("=="), _), (IntConst(ic1), _)), _), (IntConst(ic2), _))   -> (Changed, (BoolConst(ic1 == ic2), layer))
+    | Apply((Apply((PermContentOf(">="), _), (IntConst(ic1), _)), _), (IntConst(ic2), _))   -> (Changed, (BoolConst(ic1 >= ic2), layer))
+    | Apply((Apply((PermContentOf("<="), _), (IntConst(ic1), _)), _), (IntConst(ic2), _))   -> (Changed, (BoolConst(ic1 <= ic2), layer))
+    | Apply((Apply((PermContentOf(">"), _), (IntConst(ic1), _)), _), (IntConst(ic2), _))    -> (Changed, (BoolConst(ic1 > ic2), layer))
+    | Apply((Apply((PermContentOf("<"), _), (IntConst(ic1), _)), _), (IntConst(ic2), _))    -> (Changed, (BoolConst(ic1 < ic2), layer))
+    | Apply((Apply((PermContentOf("&&"), _), (BoolConst(bc1), _)), _), (BoolConst(bc2), _)) -> (Changed, (BoolConst(bc1 && bc2), layer))
+    | Apply((Apply((PermContentOf("||"), _), (BoolConst(bc1), _)), _), (BoolConst(bc2), _)) -> (Changed, (BoolConst(bc1 || bc2), layer))
+    | _                                                                                     -> (Stable, ast)
+
+
 let rec replace (ast1 : abstract_tree) (varnm : variable_name) (ast2 : abstract_tree) =
   let iter ast = replace ast varnm ast2 in
   let (astmain1, layer) = ast1 in
@@ -60,14 +77,17 @@ let rec eval (evlayer : int) (ast : abstract_tree) =
 
   (* -- non-values -- *)
 
-    | FixPoint(pvnm, ast1) ->
-        if layer = evlayer then
-          (Changed, replace ast1 pvnm ast)
-        else
-          (Stable, ast) (* ? *)
+    | FixPoint(ovnm, ast1) ->
+        if evlayer = layer then
+          (Changed, replace ast1 ovnm ast)
+        else if evlayer > layer then
+          (Stable, ast)
+        else (* if evlayer < layer *)
+          let (state1, res1) = eval evlayer ast1 in
+            (state1, (FixPoint(ovnm, res1), layer))
 
     | IfThenElse(ast0, ast1, ast2) ->
-        if layer = evlayer then
+        if evlayer = layer then
           let (state0, res0) = eval evlayer ast0 in
           begin
             match (state0, res0) with
@@ -76,8 +96,17 @@ let rec eval (evlayer : int) (ast : abstract_tree) =
             | (Stable, (BoolConst(false), _)) -> (Changed, ast2)
             | (Stable, _)                     -> assert false
           end
-        else
-          (Stable, ast) (* ? *)
+        else if evlayer > layer then
+          (Stable, ast)
+        else (* if evlayer < layer *)
+          let (state1, res1) = eval evlayer ast1 in
+          begin
+            match state1 with
+            | Changed -> (Changed, (IfThenElse(ast0, res1, ast2), layer))
+            | Stable  ->
+                let (state2, res2) = eval evlayer ast2 in
+                  (state2, (IfThenElse(ast0, res1, res2), layer))
+          end
 
     | Next(ast1) ->
         let (state1, res1) = eval evlayer ast1 in
@@ -98,14 +127,59 @@ let rec eval (evlayer : int) (ast : abstract_tree) =
           (state1, (Box(res1), layer))
 
     | Unbox(pvnm, i, ast1, ast2) ->
-        let (state1, res1) = eval evlayer ast1 in
-        begin
-          match (state1, res1) with
-          | (Changed, _)                        -> (Changed, (Unbox(pvnm, i, res1, ast2), layer))
-          | (Stable, (Box(res1sub), _))         -> (Changed, replace ast2 pvnm ast1)
-          | (Stable, (Unbox(pvnm1, j, ast11, ast12), _))
-              when not (emerge_free pvnm1 ast2) -> (Changed, (Unbox(pvnm1, i + j, ast11, (Unbox(pvnm, i, ast12, ast2), layer)), layer))
-          | (Stable, _)                         ->
-              let (state2, res2) = eval evlayer ast2 in
-                (state2, (Unbox(pvnm, i, res1, res2), layer))
-        end
+        if evlayer = layer then
+          let (state1, res1) = eval evlayer ast1 in
+          begin
+            match (state1, res1) with
+            | (Changed, _)                        -> (Changed, (Unbox(pvnm, i, res1, ast2), layer))
+            | (Stable, (Box(res1sub), _))         -> (Changed, replace ast2 pvnm ast1)
+            | (Stable, (Unbox(pvnm1, j, ast11, ast12), _))
+                when not (emerge_free pvnm1 ast2) -> (Changed, (Unbox(pvnm1, i + j, ast11, (Unbox(pvnm, i, ast12, ast2), layer)), layer))
+            | (Stable, _)                         ->
+                let (state2, res2) = eval evlayer ast2 in
+                  (state2, (Unbox(pvnm, i, res1, res2), layer))
+          end
+        else if evlayer > layer then
+          (Stable, ast)
+        else (* if evlayer < layer *)
+          let (state1, res1) = eval evlayer ast1 in
+          begin
+            match state1 with
+            | Changed -> (Changed, (Unbox(pvnm, i, res1, ast2), layer))
+            | Stable  ->
+                let (state2, res2) = eval evlayer ast2 in
+                  (state2, (Unbox(pvnm, i, res1, res2), layer))
+          end
+
+    | Apply(ast1, ast2) ->
+       if evlayer = layer then
+         let (state1, res1) = eval evlayer ast1 in
+         begin
+           match state1 with
+           | Changed -> (Changed, (Apply(res1, ast2), layer))
+           | Stable  ->
+               let (state2, res2) = eval evlayer ast2 in
+               begin
+                 match state2 with
+                 | Changed -> (Changed, (Apply(res1, res2), layer))
+                 | Stable  ->
+                     begin
+                       match res1 with
+                       | (Lambda(ovnm, ast1sub), _) -> (Changed, replace ast1sub ovnm res2)
+                       | (Unbox(pvnm, i, ast11, ast12), _)  when not (emerge_free pvnm ast2) ->
+                           (Changed, (Unbox(pvnm, i, ast11, (Apply(ast12, ast2), layer)), layer))
+                       | _ -> delta_reduction (Apply(res1, res2), layer)
+                     end
+               end
+         end
+       else if evlayer > layer then
+         (Stable, ast)
+       else (* if evlayer < layer *)
+         let (state1, res1) = eval evlayer ast1 in
+         begin
+           match state1 with
+           | Changed -> (Changed, (Apply(res1, ast2), layer))
+           | Stable  ->
+               let (state2, res2) = eval evlayer ast2 in
+                 (state2, (Apply(res1, res2), layer))
+         end
